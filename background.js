88c6cd34da4b5f08b17d3fa47248d9dcd98687e6
James D Bloom <jamesdbloom@gmail.com>
2013-11-27 09:39:16 +0000

    added 'use strict' and improve equality comparison

--- background.js
+++ background.js
@@ -1,34 +1,39 @@
-var removeAllCookies = function () {
+(function () {
+    "use strict";
 
-    if (!chrome.cookies) {
-        chrome.cookies = chrome.experimental.cookies;
-    }
+    var removeAllCookies = function () {
 
-    var removeCookie = function (cookie) {
-        var url = "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain + cookie.path;
-        chrome.cookies.remove({"url": url, "name": cookie.name});
+        if (!chrome.cookies) {
+            chrome.cookies = chrome.experimental.cookies;
+        }
+
+        var removeCookie = function (cookie) {
+            var url = "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain + cookie.path;
+            chrome.cookies.remove({"url": url, "name": cookie.name});
+        };
+
+        chrome.cookies.getAll({}, function (all_cookies) {
+            var count = all_cookies.length;
+            for (var i = 0; i < count; i++) {
+                removeCookie(all_cookies[i]);
+            }
+        });
+
+        return "CLEARED COOKIES USING EXTENSION API";
     };
 
-    chrome.cookies.getAll({}, function (all_cookies) {
-        var count = all_cookies.length;
-        for (var i = 0; i < count; i++) {
-            removeCookie(all_cookies[i]);
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        console.log(removeAllCookies());
+    });
+
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request === "CLEAR_COOKIES_EXTENSION_API") {
+            sendResponse(removeAllCookies());
+            sendResponse(removeAllCookies());
         }
     });
 
-    return "CLEARED COOKIES USING EXTENSION API";
-};
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-    console.log(removeAllCookies());
-});
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request == "CLEAR_COOKIES_EXTENSION_API") {
-        sendResponse(removeAllCookies());
-        sendResponse(removeAllCookies());
-    }
-});
+})();
 
 /* 
  to clear cookies using only those exposed to javascript run:
